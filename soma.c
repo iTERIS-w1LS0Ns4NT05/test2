2a277733275e7bbf80bc900aaf284f9a3c7c59c9
fi0ti <pw.druida@gmail.com>
2021-03-10 04:17:12 -0300

    segunda tentativa de merge com a master

---
+++ soma.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() { 
+    //declarando variaveis
+    float a, b, soma;
+    //entrada a ser guardada na primeira variavel a
+    printf("escolha um numero: ");//troca de chaves por colchetes
+    //guardando entrada no endereço de a
+    scanf("%f", &a);//inclusao do e comercial
+    //entrada a ser guardada na variavel b
+    printf("escolha um segundo numero: ");//troca de chaves por colchetes
+    //guardando entrada no endereço de b
+    scanf("%f", &b);//inclusao do e comercial
+    //operacao de somatoria
+    soma = a+b;
+    //saida do resultado para o usuario
+    printf("O resultado dos produtos: %.2f", soma);//troca de chaves por colchetes
+return 0;
+}
