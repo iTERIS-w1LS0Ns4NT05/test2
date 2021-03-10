03ecec40498140f1673aab552e73da51d917c6e1
fi0ti <pw.druida@gmail.com>
2021-03-10 00:33:57 -0300

    renomeacao do arquivo para executavel em c e correcao dos simbolos, agora salvando

--- soma.c
+++ soma.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    //declarando variaveis
     float a, b, soma;
-    printf{"escolha um numero: "};
-    scanf{"%f", a};
-    printf{"escolha um segundo numero: "};
-    scanf{"%f", b};
+    //entrada a ser guardada na primeira variavel a
+    printf("escolha um numero: ");//troca de chaves por colchetes
+    //guardando entrada no endereço de a
+    scanf("%f", &a);//inclusao do e comercial
+    //entrada a ser guardada na variavel b
+    printf("escolha um segundo numero: ");//troca de chaves por colchetes
+    //guardando entrada no endereço de b
+    scanf("%f", &b);//inclusao do e comercial
+    //operacao de somatoria
     soma = a+b;
-    printf{"O resultado dos produtos: %.2f", soma};
+    //saida do resultado para o usuario
+    printf("O resultado dos produtos: %.2f", soma);//troca de chaves por colchetes
 return 0;
 }
